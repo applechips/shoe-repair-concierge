675b6325b3202bd704f88d1bf7eaaf3f3591307d
Nicole <nhswong@gmail.com>
2017-09-16 13:06:58 -0700

    add services + styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Header, Container, Segment, Image, Grid } from 'semantic-ui-react'
+import { Image, Grid } from 'semantic-ui-react'
 import logo from './imgs/src-logo.svg';
 import contactImage from './imgs/contact-photo.jpg';
 import background from './imgs/background-edit.png';
+import brochureFront from './imgs/brochure-front.jpg';
+import brochureBack from './imgs/brochure-back.jpg';
 // import aboutImage from './imgs/about-photo.jpg';
 
 
@@ -15,12 +17,12 @@ class App extends Component {
             <div className="src-content">
 
                 {/* HEADER */}
-                <header id="header" class="alt">
+                <header id="header" className="alt">
                     <p>Shoe Repair Concierge</p>
                     <nav id="nav">
                         <ul>
                             <li><a href="index.html">Home</a></li>
-                            <li><a href="generic.html">Services</a></li>
+                            <li><a href="#services">Services</a></li>
                             <li><a href="elements.html">About</a></li>
                             <li><a href="elements.html">Contact</a></li>
                         </ul>
@@ -32,58 +34,84 @@ class App extends Component {
                     <Image src={logo} verticalAlign="middle" alt="logo" className="logo"/>
                 </section>
 
+                {/* SERVICES*/}
+                <section id="services" className="services-section">
+                    <div className="description">
+                        <p>CHINATOWN VANCOUVER LOCATED. BEST PRICES GUARANTEED. ARTISAN STYLE SHOE REPAIR.</p>
+                    </div>
+                    <div className="brochures">
+                        <Grid>
+                            <Grid.Row>
+                                <Grid.Column width={8}>
+                                    <Image src={brochureFront} className="brochure-front" />
+                                </Grid.Column>
+                                <Grid.Column width={8}>
+                                    <Image src={brochureBack} className="brochure-back" />
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </div>
+
+
+                </section>
+
+                {/* ABOUT */}
+                <section id="about" className="about-section">
+
+                </section>
+
                 {/*<section vertical masive className="home-page">*/}
 
-                    {/*<div className="navbar-menu">*/}
-                        {/*<a className="navbar">Home</a>*/}
-                        {/*<a className="navbar">Services</a>*/}
-                        {/*<a className="navbar">About</a>*/}
-                        {/*<a className="navbar">Contact</a>*/}
-                    {/*</div>*/}
+                {/*<div className="navbar-menu">*/}
+                {/*<a className="navbar">Home</a>*/}
+                {/*<a className="navbar">Services</a>*/}
+                {/*<a className="navbar">About</a>*/}
+                {/*<a className="navbar">Contact</a>*/}
+                {/*</div>*/}
 
-                    {/*<Image src={background} verticalAlign="middle" />*/}
-                    {/*<Image src={logo} verticalAlign="middle" alt="logo" className="logo"/>*/}
-                    {/*<Header>Shoe Repair Concierge</Header>*/}
+                {/*<Image src={background} verticalAlign="middle" />*/}
+                {/*<Image src={logo} verticalAlign="middle" alt="logo" className="logo"/>*/}
+                {/*<Header>Shoe Repair Concierge</Header>*/}
                 {/*</section>*/}
 
                 {/*<Segment id="home">*/}
-                    {/*<Header>Home</Header>*/}
+                {/*<Header>Home</Header>*/}
                 {/*</Segment>*/}
                 {/*<Segment id="about">*/}
-                    {/*<Grid>*/}
-                        {/*<Grid.Column width={8}>*/}
-                            {/*/!*<Image src={aboutImage}/>*!/*/}
-                        {/*</Grid.Column>*/}
-                        {/*<Grid.Column width={8} className="text-column">*/}
-                            {/*<Header>About</Header>*/}
-                            {/*<p>*/}
-                                {/*Third generation Polish, bespoke shoemaker and cobbler Michal Marcinek, studied in*/}
-                                {/*Florence, Italy at Accademia Riacci which is regarded as one of the worlds most*/}
-                                {/*prestigious artisan shoemaking schools in the world. His experience in shoe repair, orthopedic*/}
-                                {/*footwear, boot factory manufacturing and handmade footwear gives him a unique approach*/}
-                                {/*in the west coast footwear industry. All products are made, designed and sourced in*/}
-                                {/*Vancouver, B.C. resulting in functional, aesthetic and quality crafted pieces.*/}
-                            {/*</p>*/}
-                        {/*</Grid.Column>*/}
-                    {/*</Grid>*/}
+                {/*<Grid>*/}
+                {/*<Grid.Column width={8}>*/}
+                {/*/!*<Image src={aboutImage}/>*!/*/}
+                {/*</Grid.Column>*/}
+                {/*<Grid.Column width={8} className="text-column">*/}
+                {/*<Header>About</Header>*/}
+                {/*<p>*/}
+                {/*Third generation Polish, bespoke shoemaker and cobbler Michal Marcinek, studied in*/}
+                {/*Florence, Italy at Accademia Riacci which is regarded as one of the worlds most*/}
+                {/*prestigious artisan shoemaking schools in the world. His experience in shoe repair, orthopedic*/}
+                {/*footwear, boot factory manufacturing and handmade footwear gives him a unique approach*/}
+                {/*in the west coast footwear industry. All products are made, designed and sourced in*/}
+                {/*Vancouver, B.C. resulting in functional, aesthetic and quality crafted pieces.*/}
+                {/*</p>*/}
+                {/*</Grid.Column>*/}
+                {/*</Grid>*/}
                 {/*</Segment>*/}
                 {/*<Segment id="services">*/}
-                    {/*<Header>Services</Header>*/}
+                {/*<Header>Services</Header>*/}
                 {/*</Segment>*/}
                 {/*<Segment id="contact">*/}
-                    {/*<Grid>*/}
-                        {/*<Grid.Column width={8}>*/}
-                            {/*<Image src={contactImage}/>*/}
-                        {/*</Grid.Column>*/}
-                        {/*<Grid.Column width={8}>*/}
-                            {/*<Header>CONTACT</Header>*/}
-                            {/*<p>Chinatown Plaza</p>*/}
-                            {/*<p>180 Keefer Street</p>*/}
-                            {/*<p>Vancouver, BC</p>*/}
-                            {/*​*/}
-                            {/*<p>Email: marcinek@shoerepair.pl</p>*/}
-                        {/*</Grid.Column>*/}
-                    {/*</Grid>*/}
+                {/*<Grid>*/}
+                {/*<Grid.Column width={8}>*/}
+                {/*<Image src={contactImage}/>*/}
+                {/*</Grid.Column>*/}
+                {/*<Grid.Column width={8}>*/}
+                {/*<Header>CONTACT</Header>*/}
+                {/*<p>Chinatown Plaza</p>*/}
+                {/*<p>180 Keefer Street</p>*/}
+                {/*<p>Vancouver, BC</p>*/}
+                {/*​*/}
+                {/*<p>Email: marcinek@shoerepair.pl</p>*/}
+                {/*</Grid.Column>*/}
+                {/*</Grid>*/}
                 {/*</Segment>*/}
             </div>
         );
